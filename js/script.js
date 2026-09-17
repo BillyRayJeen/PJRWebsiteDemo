304d70282686a7ba0403fa5bc1fd94a2e906f9c3
Billy Green <121873603+BillyRayJeen@users.noreply.github.com>
2026-09-17 17:28:56 +0100

    Clean and replace JS

--- js/script.js
+++ js/script.js
@@ -4,20 +4,16 @@ const menuToggle = document.querySelector(".menu-toggle");
 const navigation = document.querySelector(".main-navigation");
 
 menuToggle.addEventListener("click", () => {
+    const isOpen = navigation.classList.toggle("active");
 
-navigation.classList.toggle("active");
-
+    menuToggle.setAttribute("aria-expanded", isOpen);
 });
-
 
 const navigationLinks = document.querySelectorAll(".main-navigation a");
 
 navigationLinks.forEach(link => {
-
-link.addEventListener("click", () => {
-
-navigation.classList.remove("active");
-
-});
-
+    link.addEventListener("click", () => {
+        navigation.classList.remove("active");
+        menuToggle.setAttribute("aria-expanded", "false");
+    });
 });
